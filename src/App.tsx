112d8c814933b2ef50b4ae1b4c0c89dc99b1a432
J3romey <jerome.adviento@hotmail.com>
2023-11-05 16:30:48 -0700

    fix up the files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+// import ChampionBan from './components/ChampionBan.tsx'
+import TeamContainer from './components/TeamContainer.tsx'
+import { Champion } from './interfaces/Champion.ts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [champions, setChampions] = useState<Champion[]>([])
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    fetch("http://ddragon.leagueoflegends.com/cdn/13.21.1/data/en_US/champion.json", {  signal: abortController.signal })
+    .then((response) => response.json())
+    .then(json => json.data)
+    .then(championsData => {
+        let x = new Array<Champion>();
+
+        Object.keys(championsData).forEach((key) => {
+            let championData = championsData[key]
+            let champion = new Champion;
+        
+            champion.id = championData.id;
+            champion.tags = championData.tags;
+
+            x.push(champion);
+        })
+
+        console.log(x)
+
+        setChampions(champions => champions.concat(x))
+    })
+    .catch((error) => {
+      // handle your errors here
+      console.error(error)
+    })
+    return () => {
+        // this will cancel the fetch request when the effect is unmounted
+        abortController.abort();
+        setChampions([])
+    };
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        In-House Aram
+      </h1>
+      {/* <ChampionBan values={champions}/> */}
+
+      <TeamContainer values={champions}/>
+    </div>
   )
 }
 
