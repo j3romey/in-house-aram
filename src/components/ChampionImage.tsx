acb46a881a33708b5cde43724433527325df6eb1
J3romey <jerome.adviento@hotmail.com>
2023-11-11 12:26:53 -0600

    fix champion image width

--- src/components/ChampionImage.tsx
+++ src/components/ChampionImage.tsx
@@ -4,7 +4,7 @@ function ChampionImage(champion : IChampion) {
     let url = "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + champion.id + "_0.jpg"; 
 
     return (
-        <div className="grow basis-1/5">
+        <div className="w-1/5">
             <img className='w-full' id={champion.id} src={url}/>
         </div>
     )
